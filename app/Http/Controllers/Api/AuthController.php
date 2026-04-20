4ecf51efb307746be83fb1137f8479013dcfc635
cankie7474 <cankayacbon@gmail.com>
2026-04-20 08:48:02 +0200

    feat: add register login, middleware for all routes

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register (Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => [
+                'required',
+                'string',
+                Password::min(8)
+                ->mixedCase() // Groß + Kleinbuchstaben
+                ->letters() // Buchstaben
+                ->numbers() // Zahlen
+                ->symbols(), // Sonderzeichen
+            ],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function login (Request $request){
+        $request->validate([
+            'email' =>  ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::make($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ]);
+    }
+
+    public function me(Request $request)
+    {
+        return response()->json(
+            $request->user()->only(['id', 'name', 'email'])
+        );
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,33 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CardController;
 use App\Http\Controllers\Api\DeckController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Auth
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-// Deck Controller
-Route::get('/decks', [DeckController::class, 'index']);
-Route::get('/decks/{deck}', [DeckController::class, 'show']);
-Route::post('/decks', [DeckController::class, 'store']);
-Route::delete('/decks/{deck}', [DeckController::class, 'destroy']);
-Route::patch('/decks/{deck}', [DeckController::class, 'update']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
-// Card Controller
-Route::get('/decks/{deck}/cards', [CardController::class, 'index']);
-Route::post('/decks/{deck}/cards', [CardController::class, 'store']);
-Route::delete('/decks/{deck}/cards/{card}', [CardController::class, 'destroy']);
-Route::patch('/decks/{deck}/cards/{card}', [CardController::class, 'update']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Decks
+    Route::get('/decks', [DeckController::class, 'index']);
+    Route::get('/decks/{deck}', [DeckController::class, 'show']);
+    Route::post('/decks', [DeckController::class, 'store']);
+    Route::delete('/decks/{deck}', [DeckController::class, 'destroy']);
+    Route::patch('/decks/{deck}', [DeckController::class, 'update']);
+
+    // Card
+    Route::get('/decks/{deck}/cards', [CardController::class, 'index']);
+    Route::post('/decks/{deck}/cards', [CardController::class, 'store']);
+    Route::delete('/decks/{deck}/cards/{card}', [CardController::class, 'destroy']);
+    Route::patch('/decks/{deck}/cards/{card}', [CardController::class, 'update']);
+
+});
